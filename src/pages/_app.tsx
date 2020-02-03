0711436e8b2b1fc36ae64efdaf95156609d5bc36
Nilushanan Kulasingham <nkulasingham@gmail.com>
2020-02-04 00:24:09 +1100

    feat: setup initial app structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import App, { Container } from 'next/app';
 import Head from 'next/head';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import theme from '../../config/theme';
 
-export default class MyApp extends App {
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #ECEFF4;
+    font-family: sans-serif;
+  }
+`;
+
+export default class NextApp extends App {
   public render() {
     const { Component, pageProps } = this.props;
 
     return (
       <Container>
+        <GlobalStyle />
         <Head>
           <meta
             name="viewport"
@@ -17,9 +27,11 @@ export default class MyApp extends App {
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
           />
-          <title>Next.js TypeScript Quickstart</title>
+          <title>Location, Location, Location</title>
         </Head>
-        <Component {...pageProps} />
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
       </Container>
     );
   }
