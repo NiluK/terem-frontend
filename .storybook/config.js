9b947de8919dce4990e21e0cf1c648c77b4501b8
Nilushanan Kulasingham <nkulasingham@gmail.com>
2020-02-04 04:26:31 +1100

    fix: make styled-components work with storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,8 @@
 
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import themeDecorator from "./themeDecorator"
 
 // automatically import all files ending in *.stories.tsx
 configure(require.context('../src', true, /\.stories\.tsx?$/), module);
+
+addDecorator(themeDecorator);
